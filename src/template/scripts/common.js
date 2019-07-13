d6c784020a70f0b590d82a2519d3301131afae94
Oleg Frey <olfrey@ya.ru>
2019-07-13 19:57:43 +0300

    html, text, img

--- src/template/scripts/common.js
+++ src/template/scripts/common.js
@@ -2,4 +2,17 @@ $(document).ready(function($) {
 
 	$('.input_phone .input__wrap').mask('+7 (000) 000-00-00');
 
+
+	var nav = $('.nav'),
+			navClass = 'nav_scroll';
+
+	$(window).on('scroll load resize', function() {
+		var scrollTop = $(window).scrollTop();
+
+		if (scrollTop > 130) {
+			nav.addClass(navClass);
+		} else {
+			nav.removeClass(navClass);
+		}
+	});
 });
